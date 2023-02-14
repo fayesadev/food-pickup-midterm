3c94cac1cceaf9452605bba97a44153dd23c1c67
Lauren Johnston <hilotraining@outlook.com>
2023-02-13 22:38:35 -0700

    update qty done

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -35,6 +35,7 @@ $(document).ready(function() {
     const $orderItem = createCartElement(id);
     $('.cart-items')[0].prepend($orderItem);
 
+    addUpdateQuantityEventListener();
     addRemoveItemsEventListener();
     updateCartTotal();
   });
@@ -46,10 +47,10 @@ $(document).ready(function() {
     const $cartItem = $(
       `<article class="cart-items">
       <div class="cart-row">
-        <span class="cart-col-1 cart-column name" id="${order[obj].id}">${obj.name}</span>
+        <span class="cart-col-1 cart-column name" id="${order[obj].id}">${order[obj].name}</span>
         <span class="cart-col-2 cart-column price">$${order[obj].price}</span>
         <div class="cart-col-3 cart-column qty">
-          <input class="cart-col-3-input" type="number" value="${order[obj].qty}">
+          <input class="cart-col-3-input" type="number" value="1" min="0" max="99">
           <button class="btn btn-danger" type="button">x</button>
         </div>
       </article>`
@@ -58,7 +59,7 @@ $(document).ready(function() {
     return $cartItem[0];
   };
 
-  //REMOVE ITEMS: when remove button is clicked, remove item from cart (front-end)
+  //ADD EL TO REMOVE ITEMS: adds event listener to remove items from cart
   const addRemoveItemsEventListener = () => {
     const removeCartItemButtons = $(".btn-danger");
     for (const btn of removeCartItemButtons) {
@@ -70,13 +71,37 @@ $(document).ready(function() {
     }
   };
 
+  // REMOVE ITEMS: when remove button is clicked, remove item from cart (front-end)
   const removeItemFromOrder = (item) => {
 
     item.remove(); //remove html row from browser
     const id = $(item).children().first().attr('id')[0];
-
     order[id].qty = 0;
     updateCartTotal();//will update item in the cart
+
+  };
+
+  // ADD EL TO UPDATE QTY
+  const addUpdateQuantityEventListener = () => {
+
+    const inputFields = $(".cart-col-3-input");
+    for (const input of inputFields) {
+
+      input.addEventListener('change', (e) => {
+        const id = $(e.target).parent().parent().children().attr('id')[0];
+        const qty = $(input).val()[0];
+        updateCartQuantity(id, qty);
+      });
+
+    }
+
+  };
+
+
+  // UPDATE QTY: when qty is changed in cart, update order object. qty cannot exceed 99 or go below 0
+  const updateCartQuantity = (id, qty) => {
+    order[id].qty = Number(qty);
+    updateCartTotal();
   };
 
 
@@ -90,18 +115,20 @@ $(document).ready(function() {
   };
 
   addRemoveItemsEventListener();
+
+  //PROCEED TO CHECKOUT: when button is clicked, confirmation form pops up, if no items in the cart, warning pops up
 });
 
 
 
 
 
-//when proceed to checkout button is clicked, confirmation form pops up
+
 
 //when confirm order is clicked, order object gets added to the orders table in db as a new row and customer table is also updated
 
 
-//when qty is changed in cart, update order object. qty cannot exceed 99 or go below 0
+
 
 
 //if no items in the cart, warning pups up if they click proceed to checkout
