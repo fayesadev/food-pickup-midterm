c855203ca67772c9cfd3788d9148d2ddd9398d14
Lauren Johnston <hilotraining@outlook.com>
2023-02-13 17:27:59 -0700

    made order object

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,24 +1,65 @@
 //make order object
-let order = {};
 
 
-// when add to cart is clicked, order object is updated and item is added to the cart
 $(document).ready(function() {
-  const removeCartItemButtons = $(".btn-danger");
 
+  let order = {
+    hamburger: {
+      id: 1,
+      price: 10,
+      qty: 0
+    },
+    pizza: {
+      id: 3,
+      price: 5,
+      qty: 0
+    },
+    fries: {
+      id: 2,
+      price: 15,
+      qty: 0
+    }
+  };
+
+  //ADD ITEMS: when add to cart is clicked, add item to cart
+  $(".addtocart").click((e) => {
+    const btnId = e.currentTarget.value;
+    order[btnId].qty += 1;
+    $('.cart-items')[0].prepend("<p>Test</p>");
+  });
+  //REMOVE ITEMS: when remove button is clicked, remove item from cart
+  const removeCartItemButtons = $(".btn-danger");
   const removeCartItems = () => {
     for (const btn of removeCartItemButtons) {
-      btn.addEventListener('click', (event) => {
-        const btnClicked = event.target;
+      btn.addEventListener('click', (e) => {
+        const btnClicked = e.target;
         btnClicked.parentElement.parentElement.remove();
+        updateCartTotal();
       });
     }
   };
 
   removeCartItems();
+
+  //CALCULATE TOTAL: keeps count of cart total
+  const updateCartTotal = () => {
+
+    const cart = $('.cart-items')[0];
+    //console.log("cart:",cart, cart.children);
+    for (const item of cart.children) {
+      //const priceElement = $(item).find('.price').text();
+      let qty = $(item).find('.qty').find('input').val();
+
+      const price = parseFloat($(item).find('.price').text().replace(/([^0-9\\.])/g,""));
+      let x = Number($(item).find('.price').text());
+
+      //console.log(qty);
+    }
+    //const items = $(".cart-container .cart-items .cart-row")[0];
+    //console.log(items);
+  };
 });
-//CALCULATE TOTAL: keeps count of cart total
-//REMOVE ITEMS: when remove button is clicked, remove item from cart
+
 
 
 
