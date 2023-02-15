466a6747804ee66fd73e090d4aadeab3523ed23a
Lauren Johnston <hilotraining@outlook.com>
2023-02-15 10:45:27 -0700

    fixed bugs in cart - now working on proceed to checkout

--- public/scripts/meals.js
+++ public/scripts/meals.js
@@ -1,24 +1,7 @@
-//Creates the order object from the api call - this object is used for the cart and restaurant backend
-// function Meal(meal) {
-//   this.id = meal.id;
-//   this.name = meal.name;
-//   this.qty = 0;
-//   this.price = function() {
-//     const converted = Math.floor(meal.price / 10) + 5.99;
-//     return converted.toFixed(2);
-//   }
-// }
-
-// const createOrderObject = (meal) => {
-//   const revisedPrice = Math.floor(meal.price / 10) + 5.99;
-//   const formattedPrice = revisedPrice.toFixed(2);
-//   order[meal.id] = { id: meal.id, name: meal.name, price: formattedPrice, qty: 0 };
-//   return order;
-// };
-
-// GET MENU ITEMS:
+//
+// API CALL FOR MENU: Api to get menu, render the list on the page (1)
+//
 const loadMeals = (foodCategory) => {
-  console.log(123);
   $.get(
     `https://free-food-menus-api-production.up.railway.app/${foodCategory}`
   ).then(function(res) {
@@ -27,17 +10,18 @@ const loadMeals = (foodCategory) => {
   });
 };
 
+
+
+//
+// SELECT MEALS TO SHOW ON BROWSER (2)
+//
 const renderMealList = (meals, foodCategory) => {
   $(`#${foodCategory}`).empty();
   for (const meal of meals) {
-    console.log(meals);
-
+    // -----Only render meals if they have an image(NO 404) ----- //
     $.get(meal.img)
       .then(function(res) {
         createMealItem(meal, foodCategory);
-        console.log(meal.img);
-
-        //const meal = new Meal(meal); //adds meal to order
       })
       .catch(function(err) {
         // console.log(err);
@@ -45,11 +29,14 @@ const renderMealList = (meals, foodCategory) => {
   }
 };
 
-// UPDATE MENU ITEMS: update ejs template
+
+
+//
+// UPDATE MENU ITEMS: update ejs template (3)
+//
 const createMealItem = (meal, foodCategory) => {
 
   // ------ Format Item Name ------ //
-
   const mealWords = meal.id.replace(/-/g, " ").split(" ");
   const packIndex = mealWords.indexOf("pack");
   const kitIndex = mealWords.indexOf("kit");
@@ -64,7 +51,6 @@ const createMealItem = (meal, foodCategory) => {
     .join(" ");
 
   // ----- Format Item Price ----- //
-
   const revisedPrice = Math.floor(meal.price / 10) + 5.99;
   const formattedPrice = revisedPrice.toFixed(2);
 
@@ -76,47 +62,146 @@ const createMealItem = (meal, foodCategory) => {
         <div><img class="meal-image" src="${meal.img}"></div>
       </header>
       <footer class="meal-footer">
-          <div class="price">
-              <div><strong>Price:</strong></div>
-              <div>$${formattedPrice}</div>
-            </div>
-            <button value="${meal.id}" id="${meal.id}" class="addtocart">
-              <div><i class="fas fa-cart-plus"></i> ADD TO CART</div>
-            </button>
+        <div class="price">
+          <div><strong>Price:</strong></div>
+          <div>$${formattedPrice}</div>
+        </div>
+        <button value="${meal.id}" id="${meal.id}" class="addtocart">
+          <div><i class="fas fa-cart-plus"></i> ADD TO CART</div>
+        </button>
       </footer>
-        </article>
-`);
+    </article>
+  `);
   $(`#${foodCategory}`).append(mealItem);
 
+  //  ----- When specific meal is clicked, add it to order -----//
   $(`#${meal.id}`).click((e) => {
     const id = e.currentTarget.value;
-
     const order = JSON.parse(localStorage.getItem('order'));
-    console.log(order);
+    
     if (!order[id]) {
       order[id] = { id: meal.id, name: mealName, price: formattedPrice, qty: 1 };
-      localStorage.setItem("order", JSON.stringify(order));
-
     } else {
       return alert("item already selected");
     }
 
-
-    const $orderItem = createCartElement(id);
-
-
-    //addUpdateQuantityEventListener();
-    // addRemoveItemsEventListener();
+    localStorage.setItem("order", JSON.stringify(order));
+    createCartElement(id);
     updateCartTotal();
   });
 };
 
 
+//
+//CREATE CART ELEMENT: takes in menu item object, returns a cart <article>
+//
+const createCartElement = (id) => {
+  const order = JSON.parse(localStorage.getItem('order'));
+
+  //----- show item on browser in the cart -----//
+  const $cartItem = $(
+    `
+      <div class="cart-row-" id="cart-row.${order[id].id}">
+        <span class="cart-col-1 cart-column name" id="${order[id].id}">${order[id].name}</span>
+        <span class="cart-col-2 cart-column price">$${order[id].price}</span>
+        <div class="cart-col-3 cart-column qty">
+          <input class="cart-col-3-input" id="update-cart-${order[id].id}" type="number" value="1" min="0" max="99">
+          <button class="btn btn-danger" id="remove-cart-${order[id].id}" type="button">x</button>
+        </div>
+      `
+  );
+  $('.cart-items').append($cartItem);
+
+  //----- add event listener to remove item from cart and local storage updates -----//
+  $(`#remove-cart-${order[id].id}`)[0].addEventListener('click', (e) => {
+    const btnClicked = e.target;
+    const row = btnClicked.parentElement.parentElement;
+    removeItemFromOrder(row);
+  });
+
+  //----- add event listener to update quantity and local storage updates -----//
+  $(`#update-cart-${order[id].id}`)[0].addEventListener('change', (e) => {
+    const item = e.target;
+    const qty = $(item).val()[0];
+    updateCartQuantity(id, qty);
+  });
+
+  return $cartItem[0];
+};
+
+
+//
+//CALCULATE TOTAL: takes in id (string) of menu item and tracks cart total
+//
+const updateCartTotal = () => {
+  const order = JSON.parse(localStorage.getItem('order'));
+  const total = Object.values(order).reduce((acc, cur) => {
+    return acc + (cur.price * cur.qty);
+  }, 0);
+  $('.cart-total-price').html(total.toFixed(2));
+};
+
+
+//
+// UPDATE QTY: when qty is changed in cart, update order object. qty cannot exceed 99 or go below 0
+//
+const updateCartQuantity = (id, numOfItems) => {
+  const order = JSON.parse(localStorage.getItem('order'));
+  order[id].qty = Number(numOfItems);
+  localStorage.setItem('order', JSON.stringify(order));
+  updateCartTotal();
+};
+
+
+
+//
+// REMOVE ITEMS: when remove button is clicked, remove item from cart (front-end)
+//
+const removeItemFromOrder = (item) => {
+  item.remove(); //remove html row from browser
+
+  const order = JSON.parse(localStorage.getItem('order'));
+  const id = $(item).attr('id').split('.')[1];
+  delete order[id];
+  localStorage.setItem('order', JSON.stringify(order));
+
+  //will update item in the cart
+};
+
+
+//
+// PROCEED TO CHECKOUT
+//
+$('.btn-checkout').click(function() {
+  const order = JSON.parse(localStorage.getItem('order'));
+  //----- count number of order items is > 0 -----//
+  const numOfItems = Object.values(order).reduce((acc, cur) => {
+    return acc + (cur.qty);
+  }, 1);
+
+  if (numOfItems === 0) return alert("please select an item");
+
+  //----- proceed to checkout if number of order items is > 0 -----//
+  const form = $(`#confirmation-form`);
+  console.log(form);
+  if (form.style.display === "none") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
+  }
+});
+
+
+// $('.btn-btn-checkout').click(() => proceedToCheckOut());
+
+//LOAD MEALS (1)
 $(document).ready(() => {
+  const order = {};
+  localStorage.setItem('order', JSON.stringify(order));
+
   loadMeals("brisket");
   loadMeals("fried-chicken");
   loadMeals("sandwiches");
-
 });
 
 
