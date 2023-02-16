d2dd7ab16238b8432e749fc5dac4400c825a7890
fayesadev <fayedumbrigue@gmail.com>
2023-02-16 01:27:37 +0000

    CSS styling for requests bar

--- public/scripts/restaurant-home.js
+++ public/scripts/restaurant-home.js
@@ -1,7 +1,7 @@
 /********** SAMPLE DATA OBJECTS **********/
 
-const newRequest = [
-  {
+const newRequest =
+  [{
     id: 1,
     order: {
       Dummy: 1,
@@ -13,8 +13,7 @@ const newRequest = [
     order_time: "1:15pm",
     customRequest: "No peanuts pls",
     est_completion_time: 25,
-  },
-];
+  }];
 
 const orders = [
   {
@@ -64,17 +63,10 @@ const orders = [
 const mealList = function (mealObj) {
   let string = "";
   for (let meal in mealObj) {
-    string += `<li>${mealObj[meal]}x ${meal}</li>`;
+    string += `<li class="request-text">${mealObj[meal]}x ${meal}</li>`;
   }
   return string;
 };
-/* Sample meal object
-  let mealObj = {
-    'Roast Beef': 1,
-    'Chicken Club': 2,
-    'Philly Cheesesteak': 1
-  }
-  console.log('mealList', mealList(mealObj));*/
 
 //Encodes string to become safe HTML and prevent XSS
 /*const escape = function(str) {
@@ -92,62 +84,44 @@ const createRequestElement = function (orderObj) {
   const customRequest = orderObj.customRequest;
   const meals = mealList(orderObj.order);
 
-//Mentor suggestion to render orders after clicking confirm request
-function handleClick(id){
-  console.log('ID=======>', id)
-  // go through orders, find request by ID
-  //push request object into orders array
-  // then
-  // need to call render function to rerender orders UI
-}
-
   const markup = `
     <section data-id=${orderObj.id} class="new-order-request">
-        <header>
+        <header class="request-text">
           <h3>${name}</h3>
           <h3>${orderTime}</h3>
         </header>
-        <ul>
+        <ul style="list-style: none;" class="request-text">
           ${meals}
         </ul>
-        <label>Additional comments</label>
-        <p>${customRequest}</p>
-        <form class="timeEstimate">
-          <label for="timeEstimate">How much time will this order take?</label>
-          <input name="timeEstimate" placeholder="Enter Time Estimate"></input>
+        <label class="request-text">Additional comments</label>
+        <p class="request-text">${customRequest}</p>
+        <form class="timeEstimate request-text">
+          <label for="timeEstimate">How long will this order take?</label>
+          <input name="timeEstimate" placeholder="Enter time estimate in minutes"></input>
+          <span class="error">Please enter a valid time</span>
           <button class="btn-submit" type="submit">Confirm Request</button>
         </form>
       </section>`;
 
-      //Mentor suggestion in calling the order object id to render the current order after submitting time estimate
-      // $(".timeEstimate").on('submit', function (e) {
-      //   e.stopPropagation()
-      //   e.preventDefault()
-      //   console.log('=================')
-      //   // const id = $('.new-order-request').data('data-id')
-      //   console.log('------->',orderObj.id)
-      //   // handleClick(orderObj.id)
-      // })
   return markup;
 };
 
-//Renders pending order request and appends to pending-requests-container
-const renderRequest = function (orders) {
+//Takes in array of order request objects and renders pending order request and appends to pending-requests-container
+const renderRequests = function (orders) {
   for (let order of orders) {
     const $request = createRequestElement(order);
     $("#pending-requests-container").append($request);
   }
 };
 
-const loadRequest = function () {
-  $.get("/restaurants", function () {
-    // Hardcoded loadRequest with dummy newRequest data to see it on the page
-    renderRequest(newRequest);
-  });
-};
+// const loadRequest = function () {
+//   $.get("/restaurants", function () {
+//     // Hardcoded loadRequest with dummy newRequest data to see it on the page
+//     renderRequest(newRequest);
+//   });
+// };
 
 /********** CURRENT ORDER DASHBOARD **********/
-
 
 //Creates HTML markup of current order with order-fulfilled form button
 const createOrderElement = function (orderObj) {
@@ -172,7 +146,7 @@ const createOrderElement = function (orderObj) {
         </ul>
         <label>Additional comments</label>
         <p>${customRequest}</p>
-        <form id="completed-order">
+        <form class="completedOrder">
           <button type="submit" class="btn-fulfilled">Order Fulfilled!</button>
         </form>
       </section>`;
@@ -180,54 +154,53 @@ const createOrderElement = function (orderObj) {
   return markup;
 };
 
+//For CSS testing and styling  purposes
 //Renders current order request and appends to current-order-container
-const renderOrder = function (currentOrder) {
+const renderOrders = function (currentOrder) {
   for (let order of currentOrder) {
     const $order = createOrderElement(order);
     $("#current-orders-container").append($order);
   }
 };
-// Not sure if this is the correct function to load orders
-// const loadOrder = function () {
-//   $.get("/restaurants", function () {
-//     renderOrder(newRequest);
-//   });
-// };
 
-const appendOrders = function(orderObj) {
-  $("#current-orders-container").append(createOrderElement(orderObj));
+//Appends one order to current order contianer passed in a single object array
+const appendOrder = function(orderObj) {
+  const order = orderObj[0];
+  $("#current-orders-container").append(createOrderElement(order));
 }
+
 /********** JQUERY **********/
 
-$(document).ready(function (event) {
-  renderRequest(orders);
-  renderOrder(orders);
-  // loadRequest();
-  // loadOrder();
+$(document).ready(function(event) {
+  renderRequests(orders);
+  renderOrders(orders);
 
   // Dummy button to append new requests to left contianer
-  $("#dummybutton").click(function () {
-    loadRequest();
+  $("#dummybutton").click(function() {
+    renderRequests(newRequest);
   });
 
-  $(".new-order-request").on("submit", ".timeEstimate",function (event) {
+  $(".requests").on("submit", ".timeEstimate", function(event) {
       event.preventDefault();
 
-      alert("you submitted time!");
-      const $request = $(this).parent();
-      console.log("event", event);
-      //This function appends the selected request to the current orders container, but is not good since it's not formatted to a 'current-order' format with a time estimate and still shows the time estimate form
-      appendOrders(newRequest);
-
-      $.post('/restaurants', $text)
-      .then(()=> {
-        renderOrder(newRequest);
-      })
-      .catch(err => {
-        console.log(err);
-      })
+      if ($('input').val().length === 0) {
+        event.stopImmediatePropagation();
+        $('.error').slideDown().css("display", "flex").delay(2000).slideUp();
+        return;
+      }
+      alert(`You submitted ${$('input').val()} minutes`);
+      // alert($(this).serialize());
+      $(this).parent().remove();
+      appendOrder(newRequest);
     }
   );
+
+  $(".orders").on("submit", ".completedOrder", function(event) {
+    event.preventDefault();
+    alert("you fulfilled this order!");
+
+    $(this).parent().remove();
+  })
 });
 
 // Copied Tweeter formatting to submit time estimate
